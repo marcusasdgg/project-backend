0ee4ee35469d81458c8bb24c770d9f93c9f43ab7
z5456777 <z5456777@vx10.orchestra.cse.unsw.EDU.AU>
2024-06-06 15:58:23 +1000

    added documentation for adminQuizCreate and adminQuizList

--- src/quiz.js
+++ src/quiz.js
@@ -1,4 +1,3 @@
-
 /**
  * Provide a list of all quizzes that are owned by the currently logged in user.
  
@@ -6,58 +5,26 @@
  * @returns object containing quizId and name 
  */
 function adminQuizList(authUserId ){ 
-      return{ quizzes: [
-    {
-      quizId: 1,
-      name: 'My Quiz',
-    }
-   ]
+  return{ quizzes: [
+      {
+  quizId: 1,
+  name: 'My Quiz',
+      }
+    ]
   }
 }
 
 
 /**
- * Given basic details about a new quiz, create one for the logged in user.
- 
- * @param {authUserId } authUserId 
- * @param {name } name 
- * @param {description} description 
- * @returns quizId
- */
+* Given basic details about a new quiz, create one for the logged in user.
+
+* @param {authUserId } authUserId 
+* @param {name } name 
+* @param {description} description 
+* @returns quizId
+*/
 function adminQuizCreate(authUserId, name, description){
 
-  return { 
-    quizId: 2
-  }
+              return{ quizId: 2}
 
-}
-
-function adminQuizRemove (authUserId, quizId) {
-  return {
-      
-  }
-}
-
-function adminQuizInfo (authUserId, quizId) {
-  return {
-    quizId: 1,
-    name: 'My Quiz',
-    timeCreated: 1683125870,
-    timeLastEdited: 1683125871,
-    description: 'This is my quiz',
-  }
-}
-
-/**
- * adminQuizDescriptionUpdate function stub 
- */
-function adminQuizDescriptionUpdate(authUserId, quizId, name) {
-  return {};
-}
-
-/**
- * adminQuizNameUpdate function stub
- */
-function adminQuizNameUpdate() {
-  return {};
 }
