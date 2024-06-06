757c140756cf89899f9bda1204f756069584bb73
Ayush Pathak <z5416165@ad.unsw.edu.au>
2024-06-06 14:37:08 +1000

    feat: added stub function for adminUserDetails

--- src/auth.js
+++ src/auth.js
@@ -6,3 +6,18 @@
       authUserId: 1,
     };
   }
+
+
+  // Function: adminUserDetails
+  function adminUserDetails(authUserId) {
+
+    return {
+      user: {
+        userId: 1,
+        name: 'Hayden Smith',
+        email: 'hayden.smith@unsw.edu.au',
+        numSuccessfulLogins: 3,
+        numFailedPasswordsSinceLastLogin: 1,
+      },
+    };
+  }
