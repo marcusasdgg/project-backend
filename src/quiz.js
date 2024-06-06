4facd560f5d7a72231f2351a5432030aa2a91fcc
z5456777 <z5456777@vx10.orchestra.cse.unsw.EDU.AU>
2024-06-06 15:44:30 +1000

    added documentation for adminQuizList and adminQuizCreate

--- src/quiz.js
+++ src/quiz.js
@@ -1,13 +1,29 @@
+
+/**
+ * Provide a list of all quizzes that are owned by the currently logged in user.
+ 
+ * @param {authUserId} authUserId 
+ * @returns object containing quizId and name 
+ */
 function adminQuizList(authUserId ){ 
       return{ quizzes: [
     {
       quizId: 1,
       name: 'My Quiz',
     }
-  ]
-}
+   ]
+  }
 }
 
+
+/**
+ * Given basic details about a new quiz, create one for the logged in user.
+ 
+ * @param {authUserId } authUserId 
+ * @param {name } name 
+ * @param {description} description 
+ * @returns quizId
+ */
 function adminQuizCreate(authUserId, name, description){
 
    return{ quizId: 2}
