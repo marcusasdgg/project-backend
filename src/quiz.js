d23087b020c8c1fad9a60a320530616b98273916
z5456777 <z5456777@vx20.orchestra.cse.unsw.EDU.AU>
2024-06-06 23:02:25 +1000

    new documentation added to adminQuizList and adminQuizCreate after final changes

--- src/quiz.js
+++ src/quiz.js
@@ -29,3 +29,44 @@ function adminQuizCreate(authUserId, name, description){
   return{ quizId: 2}
 
 }
+
+
+function adminQuizRemove (authUserId, quizId) {
+  return {
+      
+  }
+}
+
+function adminQuizInfo (authUserId, quizId) {
+  return {
+    quizId: 1,
+    name: 'My Quiz',
+    timeCreated: 1683125870,
+    timeLastEdited: 1683125871,
+    description: 'This is my quiz',
+  }
+}
+
+/**
+ * Update the name of the relevant quiz.
+ * @param {*} authUserId id of the user who owns the quiz
+ * @param {*} quizId if of the quiz to have it's name changed
+ * @param {*} name new name of the quiz
+ * @returns {{}} empty object 
+ */
+function adminQuizNameUpdate(authUserId, quizId, name) {
+  return {};
+}
+
+/**
+ * Update the description of the relevant quiz.
+ * @param {*} authUserId id of the user who owns the quiz
+ * @param {*} quizId id of the quiz to have it's description updated
+ * @param {*} description new description of the quiz 
+ * @returns {{}} empty object 
+ */
+function adminQuizDescriptionUpdate(authUserId, quizId, description) {
+  return {};
+}
+
+
