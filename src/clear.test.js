139302a561e96e7fdfc6d048325f534c2da92dee
marcusasdgg <marcusolachan@icloud.com>
2024-06-13 14:25:30 +1000

    Fix: fixed test for clear

--- src/clear.test.js
+++ src/clear.test.js
@@ -1,14 +1,21 @@
 //This test file is meant to test the clear function in /other.js.
 
 import clear from "./other.js"
+import adminAuthRegister from "./auth.js"
+import {expect, test} from '@jest/globals';
 
-test('Test successful clear: 2', () => {
-    expect(clear()).toStrictEqual({});
+describe('authLogin', () => {
+  describe('success Cases', () => {
+    test("registering twice with the same email but separated by clear", () => {
+      adminAuthRegister("a@gmail.com","a","a","a");
+      const retcondition = clear();
+      expect(retcondition).toStrictEqual({});
+      const ifError = adminAuthRegister("a@gmail.com","a","a","a");
+      expect(ifError).toBe(Number);
+    });
+  });
 });
 
-test('Test successful clear: 1', () => {
-    expect(clear()).tobe({});
-});
 
 //this is more or less the only tests we could do due to how simple the clear function is.
 
