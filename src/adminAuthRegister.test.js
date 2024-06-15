719ebf886c3e1d66844cbb0ecface0abd378d829
marcusasdgg <marcusolachan@icloud.com>
2024-06-15 18:05:05 +1000

    Test: added test in success cases for admin auth register.

--- src/adminAuthRegister.test.js
+++ src/adminAuthRegister.test.js
@@ -4,10 +4,10 @@ import {adminAuthRegister} from "./auth.js"
 describe("AdminAuthRegister", () => {
   describe("success Cases", () => {
     test("registering twice with the same email but separated by clear", () => {
-      adminAuthRegister("a@gmail.com","a","a","a");
+      adminAuthRegister("a@gmail.com","abcdefgh","asd","abcde");
       const retcondition = clear();
       expect(retcondition).toStrictEqual({});
-      const ifError = adminAuthRegister("a@gmail.com","a","a","a");
+      const ifError = adminAuthRegister("a@gmail.com","abcdefgh","asd","abcde");
       console.log(ifError)
       expect(ifError).toStrictEqual({authUserId: expect.any(Number)});
     });
