7109ec29fb829970cbae910eaa340704038ba473
marcusasdgg <marcusolachan@icloud.com>
2024-06-15 18:19:10 +1000

    Test: added failure cases for adminAuthRegister tests

--- src/adminAuthRegister.test.js
+++ src/adminAuthRegister.test.js
@@ -1,19 +1,38 @@
 import {clear} from "./other.js"
-import {adminAuthRegister} from "./auth.js"
+import {adminAuthLogin, adminAuthRegister, adminUserDetails} from "./auth.js"
 
 describe("AdminAuthRegister", () => {
   describe("success Cases", () => {
     test("registering twice with the same email but separated by clear", () => {
-      adminAuthRegister("a@gmail.com","abcdefgh","asd","abcde");
+      adminAuthRegister("a@gmail.com","abcdefgh1","asd","abcde");
       const retcondition = clear();
       expect(retcondition).toStrictEqual({});
-      const ifError = adminAuthRegister("a@gmail.com","abcdefgh","asd","abcde");
+      const ifError = adminAuthRegister("a@gmail.com","abcdefgh1","asd","abcde");
       console.log(ifError)
       expect(ifError).toStrictEqual({authUserId: expect.any(Number)});
     });
     
+    test("normal test case with normal inputs.", () => {
+      const id = adminAuthRegister("a@gmail.com","abcdefgh1","asd","abcde");
+      expect(id).not.toStrictEqual({error: expect.any(String)})
+      expect(adminUserDetails(id)).not.toStrictEqual({error: expect.any(String)})
+    });
+    
   });
   describe("failure Cases", () => {
-    test
+    test("failure case with everything good but invalid email.", () => {
+      const id = adminAuthRegister("a@.com","abcdefgh1","asd","abcde");
+      expect(id).not.toStrictEqual({error: expect.any(String)});
+      expect(adminUserDetails(id)).not.toStrictEqual({error: expect.any(String)});
+    });
+
+    test("failure case with everything good but email used by different user.", () => {
+      const id = adminAuthRegister("a@gmail.com","abcdefgh1","asd","abcde");
+      expect(id).not.toStrictEqual({error: expect.any(String)});
+      expect(adminUserDetails(id)).not.toStrictEqual({error: expect.any(String)});
+      const idsecond = adminAuthRegister("a@gmail.com","abcdefgh1","asd","abcde");
+      expect(idsecond).not.toStrictEqual({error: expect.any(String)});
+      expect(adminUserDetails(id)).not.toStrictEqual({error: expect.any(String)});
+    });
   });
 });
