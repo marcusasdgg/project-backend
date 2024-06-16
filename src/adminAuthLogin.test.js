03a7973f4b446bc482a3dc15b84a79f735e21a3f
Ayush Pathak <z5416165@ad.unsw.edu.au>
2024-06-16 22:40:23 +1000

    fixed bugs in test for adminAuthLogin function

--- src/adminAuthLogin.test.js
+++ src/adminAuthLogin.test.js
@@ -3,48 +3,39 @@ import { adminAuthLogin, adminAuthRegister} from "./auth";
 import { clear } from "./other";
 
 describe('testing adminAuthLogin function', () => {
-  let validEmail;
-  let validPassword;
-  let invalidEmail = 'invalidemaildoesnotexistanywhereatalllll@exist.com';
-  let invalidPassword = 'passwordDoesNotExistInTheDatabase12321';
-
   beforeEach(() => {
     clear();
-    // Register a user to get a valid email and password in the datastore
-    const { email, password } = adminAuthRegister(
-      "validuser@example.com",
-      "ValidPassword123",
-      "Bob",
-      "Jones"
-    );
-    validEmail = email;
-    validPassword = password;
   });
 
+
   describe('testing failure case', () => {
-    test('test for invalid email with valid password', () => {
-      let email = 'thisemaildoesnotexistatallanywhere@invalidemail.com';
-      let password = validPassword;
-      expect(adminAuthLogin(email, password)).toEqual({ error: expect.any(String) });
+    test('test for invalid email', () => {
+      adminAuthRegister('john@gmail.com', 'ThisisavalidPW123', 'John', 'Smith');
+      let email = 'sarah@gmail.com';
+      let password = 'ThisisavalidPW123';
+      expect(adminAuthLogin(email, password)).toStrictEqual({error: "Email address does not exist"});
     });
 
     test('test for valid email with invalid password', () => {
-      let email = validEmail;
-      let password = 'thispassworddoesnotexistatall1';
-      expect(adminAuthLogin(email, password)).toEqual({ error: expect.any(String) });
+      adminAuthRegister('sarah@gmail.com', 'passwordA2', 'sarah', 'smith')
+      let email = 'sarah@gmail.com';
+      let password = 'passwordBBB2';
+      expect(adminAuthLogin(email, password)).toStrictEqual({error: "The password is incorrect"}); //expect.any(String) 
     });
 
     test('test for invalid email and invalid password', () => {
-      let email = invalidEmail;
-      let password = invalidPassword;
-      expect(adminAuthLogin(email, password)).toEqual({ error: expect.any(String) });
+      adminAuthRegister('johhny@gmail.com', 'passwordBB2', 'johnny', 'smith')
+      let email = 'invalidjohhny@gmail.com';
+      let password = 'notpasswordBB2';
+      expect(adminAuthLogin(email, password)).toStrictEqual({error: "Email address does not exist"});
     });
   });
 
   describe('testing success case', () => {
-    test('testing for an email that is valid', () => {
-      let email = validEmail;
-      let password = validPassword;
+    test('testing for an email and password that is valid', () => {
+      adminAuthRegister('jane@gmail.com', 'validPassword123', 'sarah', 'smith')
+      let email = 'jane@gmail.com';
+      let password = 'validPassword123';
       expect(adminAuthLogin(email, password)).toEqual(expect.any(Number));
     });
   });
