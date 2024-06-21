56342762f82a18b95f46bb9555d210c0a851be17
Brandon <z5495844@ad.unsw.edu.au>
2024-06-21 18:28:41 +1000

    feat: fixed bugs in quiz.js

--- src/quiz.js
+++ src/quiz.js
@@ -1,31 +1,31 @@
 import { setData, getData } from "./dataStore";
 
 /**
- * A function that scrapes the database to see if there is a user with said ID.
- * @param {*} database
- * @param {*} authUserId
- * @returns false or reference to user object within the dataBase.users array.
+ * Searches the database to check if there is a user with the specified ID.
+ * @param {*} database The database object containing user data.
+ * @param {*} id The ID of the user to search for.
+ * @returns {*} Returns the user object if found, otherwise false.
  */
 function containsUser(database, id) {
   return database.users.find((user) => user.userId === id) || false;
 }
 
 /**
- * A function that scrapes the database to see if there is a user with said ID.
- * @param {*} database
- * @param {*} authUserId
- * @returns false or reference to user object within the dataBase.users array.
+ * Searches the database to check if there is a quiz with the specified ID.
+ * @param {Object} database The database object containing quiz data.
+ * @param {string|number} id The ID of the quiz to search for.
+ * @returns {Object|boolean} Returns the quiz object if found, otherwise false.
  */
 function containsQuiz(database, id) {
   return database.quizzes.find((quiz) => quiz.quizId === id) || false;
 }
 
 /**
- * A function that scrapes the database to see if a quiz is owned by a user.
- * @param {*} database
- * @param {*} authUserId
- * @param {*} quizId
- * @returns false or reference to user object within the dataBase.users array.
+ * Checks if a specific quiz is owned by a user in the database.
+ * @param {*} database The database object containing quiz and user data.
+ * @param {*} authUserId The ID of the user to check ownership.
+ * @param {*} quizId The ID of the quiz to check.
+ * @returns {*} Returns true if the user owns the quiz, otherwise false.
  */
 function quizOwned(database, authUserId, quizId) {
   for (const quiz of database.quizzes) {
@@ -38,12 +38,12 @@ function quizOwned(database, authUserId, quizId) {
 }
 
 /**
- * A function that scrapes the database to see if a name is unique.
- * @param {*} database 
- * @param {*} authUserId
- * @param {*} quizId
- * @param {*} name
- * @returns false or true.
+ * Checks if a quiz name is unique for a given user and quiz in the database.
+ * @param {*} database The database object containing quiz data.
+ * @param {*} authUserId The ID of the user to check against.
+ * @param {*} quizId The ID of the quiz to check against.
+ * @param {*} name The name to check for uniqueness.
+ * @returns {*} Returns false if the name is not unique, otherwise true.
  */
 function isNameUnique(database, authUserId, quizId, name) {
   for (const quiz of database.quizzes) {
@@ -67,21 +67,21 @@ function isNameUnique(database, authUserId, quizId, name) {
  */
 function adminQuizList(authUserId) {
   const database = getData();
-  const user = database.users.find(user => user.userId === authUserId);
+  const user = database.users.find((user) => user.userId === authUserId);
 
   if (!user) {
-    return { error: 'AuthUserId is not a valid user' };
+    return { error: "AuthUserId is not a valid user" };
   }
 
-  const quizzes = database.quizzes.filter(quiz => quiz.ownerId === authUserId).map(quiz => ({
-    quizId: quiz.quizId,
-    name: quiz.name,
-  }));
+  const quizzes = database.quizzes
+    .filter((quiz) => quiz.ownerId === authUserId)
+    .map((quiz) => ({
+      quizId: quiz.quizId,
+      name: quiz.name,
+    }));
 
   return { quizzes };
-
 }
-
 
 /**
 * Function: Given basic details about a new quiz, create one for the logged in user.
@@ -94,40 +94,50 @@ function adminQuizList(authUserId) {
 function adminQuizCreate(authUserId, name, description) {
   const database = getData();
 
-  if(!database.hasOwnProperty('quizzesCreated')){
-    database.quizzesCreated =0;
+  if (!database.hasOwnProperty("quizzesCreated")) {
+    database.quizzesCreated = 0;
   }
-  
-  const user = database.users.find(user => user.userId === authUserId);
+
+  const user = database.users.find((user) => user.userId === authUserId);
 
   if (!user) {
-    return { error: 'AuthUserId is not a valid user' };
+    return { error: "AuthUserId is not a valid user" };
   }
 
   if (!/^[a-zA-Z0-9 ]+$/.test(name)) {
-    return { error: 'Name contains invalid characters' };
+    return { error: "Name contains invalid characters" };
   }
 
   if (name.length < 3 || name.length > 30) {
-    return { error: 'Name is either less than 3 characters long or more than 30 characters long' };
+    return {
+      error:
+        "Name is either less than 3 characters long or more than 30 characters long",
+    };
   }
 
   if (description.length > 100) {
-    return { error: 'Description is more than 100 characters in length' };
+    return { error: "Description is more than 100 characters in length" };
   }
 
-  const quizExists = database.quizzes.find(quiz => quiz.ownerId === authUserId && quiz.name === name);
+  const quizExists = database.quizzes.find(
+    (quiz) => quiz.ownerId === authUserId && quiz.name === name
+  );
   if (quizExists) {
-    return { error: 'Name is already used by the current logged in user for another quiz' };
+    return {
+      error:
+        "Name is already used by the current logged in user for another quiz",
+    };
   }
 
   const quizId = database.quizzesCreated + 1;
-  database.quizzesCreated +=1;
+  database.quizzesCreated += 1;
   const newQuiz = {
-    quizId,
+    quizId: quizId,
     ownerId: authUserId,
-    name,
-    description,
+    name: name,
+    description: description,
+    timeCreated: Date.now(),
+    timeLastEdited: Date.now(),
   };
 
   database.quizzes.push(newQuiz);
@@ -136,35 +146,53 @@ function adminQuizCreate(authUserId, name, description) {
   return { quizId };
 }
 
-
 /**
  * This function removes a quiz using userId.
- * 
- * @param {*} authUserId 
- * @param {*} quizId 
- * @returns 
+ *
+ * @param {*} authUserId
+ * @param {*} quizId
+ * @returns
  */
 function adminQuizRemove(authUserId, quizId) {
-  return {
+  const database = getData();
+  const user = database.users.find((user) => user.userId === authUserId);
+  const quiz = database.quizzes.find((quiz) => quiz.quizId === quizId);
 
+  if (!quiz & !user) {
+    return { error: "invalid userID & quizID" };
   }
+  if (!user) {
+    return { error: "invalid userID" };
+  }
+  if (!quiz) {
+    return { error: "invalid quizID" };
+  }
+
+  if (!quizOwned(database, authUserId, quizId)) {
+    return { error: "quizId is not owned by authUserId." };
+  }
+
+  database.quizzes = database.quizzes.filter((q) => q.quizId !== quizId);
+  setData(database);
+
+  return {};
 }
 
 /**
  * This function stores quiz info.
- * 
- * @param {*} authUserId 
- * @param {*} quizId 
+ *
+ * @param {*} authUserId
+ * @param {*} quizId
  * @returns returns a object containing info about the quiz in question.
  */
 function adminQuizInfo(authUserId, quizId) {
   return {
     quizId: 1,
-    name: 'My Quiz',
+    name: "My Quiz",
     timeCreated: 1683125870,
     timeLastEdited: 1683125871,
-    description: 'This is my quiz',
-  }
+    description: "This is my quiz",
+  };
 }
 
 /**
@@ -172,7 +200,7 @@ function adminQuizInfo(authUserId, quizId) {
  * @param {*} authUserId id of the user who owns the quiz
  * @param {*} quizId if of the quiz to have it's name changed
  * @param {*} name new name of the quiz
- * @returns {{}} empty object 
+ * @returns {{}} empty object
  */
 function adminQuizNameUpdate(authUserId, quizId, name) {
   let data = getData();
@@ -212,8 +240,8 @@ function adminQuizNameUpdate(authUserId, quizId, name) {
  * Update the description of the relevant quiz.
  * @param {*} authUserId id of the user who owns the quiz
  * @param {*} quizId id of the quiz to have it's description updated
- * @param {*} description new description of the quiz 
- * @returns {{}} empty object 
+ * @param {*} description new description of the quiz
+ * @returns {{}} empty object
  */
 function adminQuizDescriptionUpdate(authUserId, quizId, description) {
   let data = getData();
@@ -245,5 +273,11 @@ function adminQuizDescriptionUpdate(authUserId, quizId, description) {
   return {};
 }
 
-
-export { adminQuizCreate, adminQuizList, adminQuizDescriptionUpdate, adminQuizInfo, adminQuizRemove, adminQuizNameUpdate }
+export {
+  adminQuizCreate,
+  adminQuizList,
+  adminQuizDescriptionUpdate,
+  adminQuizInfo,
+  adminQuizRemove,
+  adminQuizNameUpdate,
+};
