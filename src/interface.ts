7c2d7f0ab3b3ef8261becbb178f42cd5407b9c9c
Marcus <z5479406@ad.unsw.edu.au>
2024-06-27 15:24:15 +1000

    fix: implemented interface.ts to store every interface

---
+++ src/interface.ts
@@ -0,0 +1,33 @@
+interface quiz {
+  quizId: number,
+  ownerId: number,
+  name: string,
+  description: string,
+  timeCreated: number,
+  timeLastEdited: number,
+}
+  
+interface user {
+  lastName: string,
+  username: string,
+  password: string,
+  userId: number,
+  email: string,
+  numSuccessfulLogins: number,
+  numFailedPasswordsSinceLastLogin: number
+}
+  
+interface data {
+  users: user[],
+  quizzes: quiz[],
+  usersCreated: number,
+  quizzesCreated: number,
+}
+
+interface error {
+  error: string,
+}
+
+
+
+export {data, user, quiz}
