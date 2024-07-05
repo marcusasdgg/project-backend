daa3cc380791f8ac255a618057746f5248ea2aae
Ayush Pathak <z5416165@ad.unsw.edu.au>
2024-07-05 19:22:15 +1000

    removed adminauthLogin helper function due to bug

--- src/adminAuthLogin.test.ts
+++ src/adminAuthLogin.test.ts
@@ -2,7 +2,6 @@ import { describe, expect, test, beforeEach } from "@jest/globals";
 import { adminAuthLogin, adminAuthRegister } from "./auth";
 import { clear } from "./other";
 import { sessionIdToken} from "./interface";
-import {adminAuthRegisterHelper, adminAuthLoginHelper} from "./httpHelperFunctions"
 
 describe("testing adminAuthLogin function", () => {
   beforeEach(() => {
@@ -11,7 +10,7 @@ describe("testing adminAuthLogin function", () => {
 
   describe("testing failure case", () => {
     test("test for invalid email", () => {
-      adminAuthRegisterHelper("john@gmail.com", "ThisisavalidPW123", "John", "Smith");
+      adminAuthRegister("john@gmail.com", "ThisisavalidPW123", "John", "Smith");
       let email = "sarah@gmail.com";
       let password = "ThisisavalidPW123";
       expect(adminAuthLogin(email, password)).toStrictEqual({
@@ -20,7 +19,7 @@ describe("testing adminAuthLogin function", () => {
     });
 
     test("test for valid email with invalid password", () => {
-      adminAuthRegisterHelper("sarah@gmail.com", "passwordA2", "sarah", "smith");
+      adminAuthRegister("sarah@gmail.com", "passwordA2", "sarah", "smith");
       let email = "sarah@gmail.com";
       let password = "passwordBBB2";
       expect(adminAuthLogin(email, password)).toStrictEqual({
@@ -29,7 +28,7 @@ describe("testing adminAuthLogin function", () => {
     });
 
     test("test for invalid email and invalid password", () => {
-      adminAuthRegisterHelper("johhny@gmail.com", "passwordBB2", "johnny", "smith");
+      adminAuthRegister("johhny@gmail.com", "passwordBB2", "johnny", "smith");
       let email = "invalidjohhny@gmail.com";
       let password = "notpasswordBB2";
       expect(adminAuthLogin(email, password)).toStrictEqual({
@@ -40,7 +39,7 @@ describe("testing adminAuthLogin function", () => {
 
   describe("testing success case", () => {
     test("testing for an email and password that is valid", () => {
-      adminAuthRegisterHelper("jane@gmail.com", "validPassword123", "sarah", "smith");
+      adminAuthRegister("jane@gmail.com", "validPassword123", "sarah", "smith");
       let email = "jane@gmail.com";
       let password = "validPassword123";
       expect(adminAuthLogin(email, password)).toStrictEqual({
