968e0321e895be36d0f50f4f902cc05921cf7c29
marcusasdgg <marcusolachan@icloud.com>
2024-07-06 21:22:19 +1000

    feat: implmeneted helper functions for clear, passworduipdate and detailsupdate

--- src/httpHelperFunctions.ts
+++ src/httpHelperFunctions.ts
@@ -26,6 +26,48 @@ function adminAuthRegisterHelper(email: string, password: string, nameFirst: str
   }
 }
 
+function clearHelper(): {} {
+  const res = request('DELETE','${url}:${port}/v1/clear');
+  return res;
+}
+
+function adminUserDetailsUpdateHelper(sessionId: number, email : string, nameFirst : string, nameLast : string): error | {} {
+  const body = {
+    token: sessionId,
+    email: email,
+    nameFirst: nameFirst,
+    nameLast: nameLast
+  }
+  const res = request('PUT', `${url}:${port}/v1/admin/userdetails`, {
+    json: body
+  });
+
+  if ('error' in res){
+    return res;
+  } else {
+    return {};
+  }
+}
+
+function adminUserPasswordUpdateHelper(sessionId : number, oldPassword : string, newPassword : string){
+  const body = {
+    token: sessionId,
+    oldPassword: oldPassword,
+    newPassword: newPassword,
+  }
+
+  const res = request('PUT', `${url}:${port}/v1/admin/password`, {
+    json: body
+  });
+
+  if ('error' in res){
+    return res;
+  } else {
+    return {};
+  }
+}
+
+
 
 
 export {adminAuthRegisterHelper};
