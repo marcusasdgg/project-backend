1267d29b563b8b20d5c29bd5489b8fd2f411bbb9
Cathy Dinh <z5583357@ad.unsw.edu.au>
2024-07-21 23:55:56 +1000

    feat: implemented tests for adminAuthLogout V2 coverage

--- src/adminAuthLogout.test.ts
+++ src/adminAuthLogout.test.ts
@@ -11,36 +11,34 @@ describe('adminAuthLogout', () => {
 
   beforeEach(() => {
     clearHelper();
+    const registerResponse = adminAuthRegisterHelper(
+      'user1@tookah.com',
+      'Badpasswordbad1',
+      'Bat',
+      'Batman'
+    );
+    if ('sessionId' in registerResponse) {
+      sessionId = registerResponse.sessionId;
+    }
   });
 
   describe('Successsful Cases', () => {
     test('logging out', () => {
-      const registerResponse = adminAuthRegisterHelper(
-        'user1@tookah.com',
-        'Badpasswordbad1',
-        'Bat',
-        'Batman'
-      );
-      if ('sessionId' in registerResponse) {
-        sessionId = registerResponse.sessionId;
-      }
       expect(adminAuthLogoutHelper(sessionId)).toStrictEqual({});
+    });
+    test('V2 logging out', () => {
+      expect(adminAuthLogoutV2Helper(sessionId)).toStrictEqual({});
     });
   });
 
   describe('Failure Cases', () => {
     test('token is invalid (does not refer to valid logged in user session)', () => {
-      const registerResponse = adminAuthRegisterHelper(
-        'user1@tookah.com',
-        'Badpasswordbad1',
-        'Bat',
-        'Batman'
-      );
-      if ('sessionId' in registerResponse) {
-        sessionId = registerResponse.sessionId;
-      }
       const invalidSessionId = sessionId + 1;
       expect(adminAuthLogoutHelper(invalidSessionId)).toStrictEqual({ error: expect.any(String) });
+    });
+    test('V2 token is invalid (does not refer to valid logged in user session)', () => {
+      const invalidSessionId = sessionId + 1;
+      expect(adminAuthLogoutV2Helper(invalidSessionId)).toStrictEqual({ error: expect.any(String) });
     });
   });
 });
